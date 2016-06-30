d3b61aa26fec855a8620089ad85a243bf79fac2c
Seth Tippetts <sethtippetts@gmail.com>
2016-06-30 02:15:39 -0600

    Refactor for readability

--- sdk/lib/build.js
+++ sdk/lib/build.js
@@ -6,17 +6,18 @@ const path = require('path');
 
 const template = Hogan.compile(FileSystem.readFileSync(path.resolve(__dirname, 'template.hjs'), 'utf8'));
 
-module.exports = function(container, functions) {
-  functions = functions.map(fn => {
-    fn.argumentString = fn.arguments.join(', ');
-    console.log(fn['function']);
-    fn['function'] = fn['function'].replace(/\n/gi, '\\n').replace(/"/gi, '\\"');
-    console.log(fn['function']);
+module.exports = function(container, methods) {
+  methods = methods.map(fn => {
+    fn.code = fn
+      .code
+      .toString()
+      .replace(/\n/gi, '\\n')
+      .replace(/"/gi, '\\"');
     return fn;
   })
   return template.render({
     container,
-    functions,
+    methods,
     lifecycle: container.lifecycle,
   });
 }
