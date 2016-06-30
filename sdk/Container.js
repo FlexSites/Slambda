5b32f4eebecec97c526c48ad824610e069583af9
Seth Tippetts <sethtippetts@gmail.com>
2016-06-30 02:11:24 -0600

    Remove unused logging code

--- sdk/Container.js
+++ sdk/Container.js
@@ -3,7 +3,6 @@
 const Bluebird = require('bluebird');
 
 const debug = require('debug')('slambda:Container');
-const Method = require('./Method');
 
 module.exports = class Container {
   constructor(id, storage, executor, options) {
@@ -20,39 +19,22 @@ module.exports = class Container {
   method(id, code) {
     debug(`#method() ID: ${id} Code: ${code.toString()}`);
     let wait = this.storage.putMethod({ id, container: this.id, code })
-      .then((results) => {
-        console.log('method success', results);
-      })
     if (this.options.autoDeploy) {
-      console.log('autoDeploy');
       wait
-      .then(() => {
-        console.log('thened');
-        this.deploy();
-      });
+        .then(() => this.deploy())
+        .catch(ex => console.error(ex));
     }
     return this;
   }
 
   deploy() {
     debug(`#deploy()`);
-    this.storage.getContainer(this.id)
-      .then(container => console.log('CONTAINER', container))
-      .catch(ex => console.error(ex));
-
-    this.storage.listMethods(this.id)
-      .then(container => console.log('METHODS', container))
-      .catch(ex => console.error(ex));
-
     return Bluebird.all([
       this.storage.getContainer(this.id),
       this.storage.listMethods(this.id),
     ])
-    .tap(results => console.log('deploy fetch'))
-    .spread((container, methods) => {
-      console.log('deploying', container, methods);
-      return this.executor.deploy(container, methods);
-    })
+    .spread(this.executor.deploy.bind(this.executor))
+    .catch(ex => console.error(ex));
   }
 }
 
