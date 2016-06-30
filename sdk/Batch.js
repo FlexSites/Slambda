c11a825f75138ffacbb8b5a6a159cf82d2f31ed8
Seth Tippetts <sethtippetts@gmail.com>
2016-06-30 02:11:05 -0600

    Change to only flush when new calls are on the queue

--- sdk/Batch.js
+++ sdk/Batch.js
@@ -2,16 +2,21 @@
 
 const Bluebird = require('bluebird');
 
+const debug = require('debug')('slambda:Container');
+
 module.exports = class Batch {
   constructor(executor, manual) {
+    debug('constructor');
     this.inputs = [];
     this.promises = [];
     this.executor = executor;
     this.manual = manual;
-    if (!manual) this.auto();
+
+    this.flushing = false;
   }
 
   run(id, args) {
+    debug(`#run() ID: ${id}`);
     if (!this.manual) this.auto();
     this.inputs.push({ id, arguments: args });
     return new Bluebird((resolve, reject) => {
@@ -20,10 +25,17 @@ module.exports = class Batch {
   }
 
   auto() {
+    if (this.flushing) return;
+
+    debug(`#auto() Setting up nextTick flush`);
     process.nextTick(() => this.flush());
+    this.flushing = true;
   }
 
   flush() {
+    debug(`#flush() ${this.inputs.length} queued calls`);
+
+    this.flushing = false;
     let inputs = this.inputs;
     let promises = this.promises;
     this.inputs = [];
